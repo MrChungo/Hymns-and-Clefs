61287acfe18de7d4df54cf3d3d7ce19d5a98d94d
MrChungo <elchungo0518@gmail.com>
2026-01-16 12:55:06 -0500

    forgot to save TT

--- hymns-&-clefs-(base-game)/Scripts/Managers/input_manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/Managers/input_manager.gd
@@ -9,3 +9,27 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
+func _input(event):
+	#checks list of all events (key inputs)
+	#checks the type of event (use this for later reference)
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			raycast_at_cursor()
+		else:
+			
+
+
+func raycast_at_cursor():
+	#checks if card is below mouse position
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		var result_collision_mask = result[0].collider.get_parent()
+	else:
+		return null
