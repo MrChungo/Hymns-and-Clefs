01af2a1850415d2400cf76a355d6dac7635fa31e
MrChungo <elchungo0518@gmail.com>
2026-01-22 12:06:16 -0500

    Version 1 of Hand & deck done
    don't have a way to add new cards yet or remove but things are slowly working out. have a way for cards to eventually do something, things progressing well :3

--- hymns-&-clefs-(base-game)/Scripts/deck_and_card_scripts/card_manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/deck_and_card_scripts/card_manager.gd
@@ -45,7 +45,7 @@ func finish_drag():
 		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
 		card_slot_found.card_in_slot = true
 	else:
-		player_hand_reference.add_card_to_hand(card_being_dragged)
+		player_hand_reference.add_card_to_hand(card_being_dragged, card_being_dragged.position_in_hand)
 	card_being_dragged = null
 	
 	
