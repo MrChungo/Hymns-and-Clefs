3fca476256ebb48713463603a647ec3b081322d2
MrChungo <elchungo0518@gmail.com>
2026-02-04 11:59:40 -0500

    awawa

--- hymns-&-clefs-(base-game)/Scripts/Resource_scripts/enemy_resource.gd
+++ hymns-&-clefs-(base-game)/Scripts/Resource_scripts/enemy_resource.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name enemy_resource
 
-@export var texture: Texture2D
+#@export var AnimatedSprite: AnimatedSprite2D
 
 @export var is_boss_enemy : bool
 @export var base_enemy_attack: int
