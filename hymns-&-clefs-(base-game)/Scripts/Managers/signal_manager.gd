e8a5e90968b41aaa4577ad2550d1b2ad79419f5d
MrChungo <elchungo0518@gmail.com>
2026-02-26 14:18:22 -0500

    changes :3

--- hymns-&-clefs-(base-game)/Scripts/Managers/signal_manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/Managers/signal_manager.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_battle_manager_battle_complete() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("uid://cri5a32uv57us")#"res://Scenes/Areas/battle.tscn"
