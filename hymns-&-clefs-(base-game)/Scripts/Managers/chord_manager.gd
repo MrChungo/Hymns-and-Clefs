2cca7928dbf108ce77d87503caea568ef65df386
MrChungo <elchungo0518@gmail.com>
2026-03-02 12:54:29 -0500

    started

--- hymns-&-clefs-(base-game)/Scripts/Managers/chord_manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/Managers/chord_manager.gd
@@ -1,1 +1,24 @@
 extends Node
+
+const NOTES_WITH_SHARPS = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
+
+const NOTES_WITH_FLATS = ["A","Bb","B","C","Db","D","Eb","E","F","Gb","G","Ab"]
+
+
+func gen_chord():
+		var chord_type = "major"
+		var root = ""
+		var third = ""
+		var fifth = ""
+		'''
+		Major (triad)
+		Minor (triad)
+		Augmented
+		Diminished
+		
+		7th?
+		'''
+		if chord_type == "major":
+			pass
+		
+		pass
