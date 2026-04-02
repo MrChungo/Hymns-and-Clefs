22427f1148723ed70cbbef9e014b3ec49a99bc76
MrChungo <elchungo0518@gmail.com>
2026-04-02 14:08:31 -0400

    awaw

--- hymns-&-clefs-(base-game)/Scripts/Managers/Save_Manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/Managers/Save_Manager.gd
@@ -5,15 +5,16 @@ const save_location = "user://SaveFile.tres"
 
 const test_save_file = "uid://chmnudsaqsjho" #"res://Resources/Save States/Test_Battle_save.tres"
 
-const SAVE_FILE_TEMPLATE_LOCATION = "uid://dbeyhpubeidew" #"res://Resources/Save States/Save_Templates/Default_Save_Template.tres"
+const SAVE_FILE_TEMPLATE_LOCATION = "res://Resources/Save States/Save_Templates/Default_Save_Template.tres"
 
 var save_file_data: save_resource
-var save_file_template: save_resource = load(SAVE_FILE_TEMPLATE_LOCATION)
+var save_file_template: save_resource = preload(SAVE_FILE_TEMPLATE_LOCATION)
 
 
 
 func _new_save():
-	ResourceSaver.save(save_file_template, save_location)
+	var data = save_file_template.duplicate(true)
+	ResourceSaver.save(data, save_location)
 	
 # Called when the node enters the scene tree for the first time.
 func _save():
@@ -22,7 +23,7 @@ func _save():
 func _load():
 	#_new_save()
 	if FileAccess.file_exists(save_location):
-		save_file_data = ResourceLoader.load(save_location).duplicate(true)
+		save_file_data = ResourceLoader.load(save_location, "", ResourceLoader.CACHE_MODE_REPLACE).duplicate(true)
 	else:
-		_new_save()
-		save_file_data = ResourceLoader.load(save_location).duplicate(true)
+		await _new_save()
+		save_file_data = ResourceLoader.load(save_location, "", ResourceLoader.CACHE_MODE_REPLACE).duplicate(true)
