19a489f24e00e0b0c78dacb0114ff2a0f92425fc
MrChungo <elchungo0518@gmail.com>
2026-04-02 13:56:21 -0400

    fix part 1

--- hymns-&-clefs-(base-game)/Scripts/Managers/signal_manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/Managers/signal_manager.gd
@@ -2,8 +2,6 @@ extends Node2D
 	
 
 func change_scene_to_map():
-	if (len(SaveManager.save_file_data.map_icons)) == SaveManager.save_file_data.current_icon:
-		SaveManager.save_file_data.world_difficulty += 1
 	SaveManager._save()
 	get_tree().change_scene_to_file("uid://cbiowlor3xmxe") #"res://Scenes/Areas/map/map.tscn"
 	
@@ -11,4 +9,6 @@ func change_scene_to_battle():
 	get_tree().change_scene_to_file("uid://cri5a32uv57us")#"res://Scenes/Areas/battle.tscn"
 
 func change_scene_to_rewards():
+	if (len(SaveManager.save_file_data.map_icons)) == SaveManager.save_file_data.current_icon:
+		SaveManager.save_file_data.world_difficulty += 1
 	get_tree().change_scene_to_file("uid://b2qnuy73pfn82") #"res://Scenes/Areas/rewards_screen.tscn"
