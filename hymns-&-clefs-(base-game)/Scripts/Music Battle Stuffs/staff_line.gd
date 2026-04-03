f6eea0d3610d806481ac20c085069a85a9086e41
MrChungo <elchungo0518@gmail.com>
2026-04-03 04:22:23 -0400

    things spawn rawr

---
+++ hymns-&-clefs-(base-game)/Scripts/Music Battle Stuffs/staff_line.gd
@@ -0,0 +1,11 @@
+extends Node2D
+class_name staffLine
+
+
+var line_defined_note: String
+
+var notes_being_held: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
