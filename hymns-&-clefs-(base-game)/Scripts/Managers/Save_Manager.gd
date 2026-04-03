bb858970dbc077ccba3fe05ec1b3b2df84d56930
MrChungo <elchungo0518@gmail.com>
2026-04-03 00:18:07 -0400

    fixed saving system

--- hymns-&-clefs-(base-game)/Scripts/Managers/Save_Manager.gd
+++ hymns-&-clefs-(base-game)/Scripts/Managers/Save_Manager.gd
@@ -14,13 +14,13 @@ var save_file_template: save_resource = preload(SAVE_FILE_TEMPLATE_LOCATION)
 
 func _new_save():
 	var data = save_file_template.duplicate(true)
-	data.deck.deck_resource = data.deck.deck_resource.duplicate()
+	data.deck = data.deck.duplicate(true)
 	ResourceSaver.save(data, save_location)
 	_load()
 	
 # Called when the node enters the scene tree for the first time.
 func _save():
-	ResourceSaver.save(save_file_data, save_location)
+	ResourceSaver.save(save_file_data.duplicate(true), save_location)
 	_load()
 
 func _load():
